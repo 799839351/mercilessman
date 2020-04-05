b51d7b84564d564e4e99952128704c7f0bb484f9
tanxiaokang <tanxiaokang@cmbchina.com>
2020-04-05 12:46:54 +0800

    添加高可用消息队列

--- mercilessman-framework-mq/src/main/java/kafka/integration/CompositeHLClient.java
+++ mercilessman-framework-mq/src/main/java/kafka/integration/CompositeHLClient.java
@@ -16,26 +16,26 @@ public class CompositeHLClient extends DefaultHLClient {
 
     public void start() {
         if (null == this.getSender()) {
-            //Sender sender = ServiceLoader.load(Sender.class).iterator().next();
-            Sender sender =new kafka.componet.sender.KafkaSender();
+            Sender sender = ServiceLoader.load(Sender.class).iterator().next();
+            //Sender sender =new kafka.componet.sender.KafkaSender();
             this.setSender(sender);
         }
 
         if (null == this.getRecorder()) {
-           // Recorder recorder = ServiceLoader.load(Recorder.class).iterator().next();
-            Recorder recorder =new kafka.componet.recorder.KafkaRecorder();
+            Recorder recorder = ServiceLoader.load(Recorder.class).iterator().next();
+            //Recorder recorder =new kafka.componet.recorder.KafkaRecorder();
             this.setRecorder(recorder);
         }
 
         if (null == this.getReceiver()) {
-           // Receiver receiver = ServiceLoader.load(Receiver.class).iterator().next();
-            Receiver receiver=new kafka.componet.receiver.KafkaReceiver();
+            Receiver receiver = ServiceLoader.load(Receiver.class).iterator().next();
+            //Receiver receiver=new kafka.componet.receiver.KafkaReceiver();
             this.setReceiver(receiver);
         }
 
         if (null == this.getStorage()) {
-            //Storage storage = ServiceLoader.load(Storage.class).iterator().next();
-            Storage storage=new kafka.componet.storage.LinkedLogStorage();
+            Storage storage = ServiceLoader.load(Storage.class).iterator().next();
+            //Storage storage=new kafka.componet.storage.LinkedLogStorage();
             this.setStorage(storage);
         }
 
